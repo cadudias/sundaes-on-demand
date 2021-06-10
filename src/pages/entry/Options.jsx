46443207cff797df62966766a7fb0add693bbb63
Ricardo Hofstetter Dias <ricardo.dias@lambda3.com.br>
2021-06-10 12:09:00 -0300

    fixed tests errors and add toppings component

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,3 +1,33 @@
-export default function Options({optionType}) {
-  return <div />;
+import { useEffect, useState } from "react"
+import axios  from "axios"
+import Row from 'react-bootstrap/Row'
+import ScoopOption from "./ScoopOption"
+import ToppingOption from "./ToppingOption"
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([])
+
+  // will run once on compoennt mount and
+  // again when optionType changes
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error
+      })
+  }, [optionType])
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption
+
+  // creates a componente for each scoop otpion
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>
 }
